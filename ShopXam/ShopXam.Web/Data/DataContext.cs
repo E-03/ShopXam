28670b555a2b6a9467000e63b3451019fd4a03d6
Eber Fabal <efabal@mardom.com>
2019-03-10 00:20:27 -0400

    Adicionando Product Controller

---
+++ ShopXam/ShopXam.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopXam.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopXam.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Prooducto { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
